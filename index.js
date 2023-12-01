d61c5ae55bd52cb84ada8e9b875a9e10ec8af77e
Vishnu A B <vishnuab1207@gmail.com>
2023-12-01 16:27:22 +0530

    cors added

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
